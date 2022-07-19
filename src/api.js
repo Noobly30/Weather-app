36a1f92c35fb509731a30af3b2b799bf43151d43
Noobly30 <jamesrobbins4024@gmail.com>
2022-07-19 16:28:35 +0800

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '979260af48msh58ebafd502a8cb4p13597ajsn609069d9cde2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = 'e99d4834b9bef05c46245b1568149bff';
